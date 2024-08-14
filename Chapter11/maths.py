cd245f937eaef8e5576303e74e6266d4da7cb1e2
JirlOP <jorge.diazsagot@ucr.ac.cr>
2024-08-14 07:45:17 -0600

    feat: Refactor division function to allow decimal precision control
    The code changes in this commit refactor the `division` function in the `Maths` class to allow control over the decimal precision of the result. The `Maths` class now has an additional parameter `decimals` in its constructor, which specifies the number of decimal places to round the result to. The `division` method has been updated to use this parameter and return the result with the specified decimal precision. This change improves the flexibility and usability of the `Maths` class.

--- Chapter11/maths.py
+++ Chapter11/maths.py
@@ -1,8 +1,15 @@
+class Maths:
+    """Math operations"""
 
-def division(a, b):
-    # if a or b is not a number, raise a ValueError
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise ValueError("Invalid input")
-    if b == 0:
-        raise ZeroDivisionError("Division by zero")
-    return a / b
+    def __init__(self, decimals = 2):
+        self.decimals = decimals
+
+    def division(self, a, b):
+        """Divide two numbers"""
+        # if a or b is not a number, raise a ValueError
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise ValueError("Invalid input")
+        if b == 0:
+            raise ZeroDivisionError("Division by zero")
+        # show only the decimals specified
+        return round(a / b, self.decimals)
--- Chapter11/test_Maths.py
+++ Chapter11/test_Maths.py
@@ -1,27 +1,52 @@
+"""
+Fixture example
+"""
 
-from Maths import division
+import pytest
+from maths import Maths
 
-def test_division_whenInputIsValid_ShouldReturnRightValue():
+# define fixture for maths
+@pytest.fixture
+def maths():
+    # put the setup code here
+    decimals = 3
+    maths = Maths(decimals)
+    return maths
+
+
+def test_division_whenInputIsValid_ShouldReturnRightValue(maths):
     # arrange
     a = 2
     b = 2
 
     # act
-    result = division(a, b)
+    result = maths.division(a, b)
 
     # assert
     assert result == 1
 
 
-def test_division_whenInputIsNotANumber_ShouldRaiseValueError():
+def test_division_whenInputIsNotANumber_ShouldRaiseValueError(maths):
     # arrange
     a = "a"
     b = 2
 
     # act and assert
     try:
-        division(a, b)
+        maths.division(a, b)
         assert False
     except ValueError:
         assert True
 
+
+def test_division_whenInputValid_shouldReturnGoodDecimalPrecision(maths):
+    # arrange
+    a = 2
+    b = 3
+
+    # act
+    result = maths.division(a, b)
+
+    # assert
+    # presicion is 3
+    assert result == 0.667
